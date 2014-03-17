f4022d6811bb4538abe6d0c41e63c31b42360ffe
Yvette White <ywhite@42six.com>
2014-03-17 18:55:53 -0400

    Transition to from jee to mongo and solely rest via jaxrs.

--- src/main/java/com/gima/feed/domain/AbstractEntity.java
+++ src/main/java/com/gima/feed/domain/AbstractEntity.java
@@ -9,7 +9,6 @@ import javax.persistence.Version;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
